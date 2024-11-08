a2c5b6f4813e07d3ac6bc7809100b0b57c50905c
Kaifkhan001 <kaifkhanksi7544@gmail.com>
2024-11-08 21:33:20 +0530

    localstorage problem

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,10 +1,8 @@
 import { create } from "zustand";
 
 const useStore = create((set) => ({
-  isLogged: JSON.parse(localStorage.getItem("isLogged")),
+  isLogged: typeof window !== "undefined" ? JSON.parse(localStorage.getItem("isLogged")) : false,
   toggleLogg: (status) => set({ isLogged: status }),
 }));
-
-
 
 export default useStore;
